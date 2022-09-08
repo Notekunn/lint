a7faad985f99ac253061373f4461e1fb985d619a
DC <cuong.nl5.kma@gmail.com>
2022-09-08 21:35:16 +0700

    feat: add nest config eslint

--- packages/eslint/index.js
+++ packages/eslint/index.js
@@ -14,5 +14,9 @@ module.exports = {
   rules: {
     'comma-dangle': ['error', 'always-multiline'],
     'space-before-function-paren': ['off'],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
 }
